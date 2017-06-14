23dcebd30b801f96171328b0fe309c11339d3b55
Ryan Gaus <rsg1egoman@gmail.com>
2017-06-14 09:16:13 -0400

    chore: add spec generator docs

--- utils/build-specs.js
+++ utils/build-specs.js
@@ -1,3 +1,4 @@
+// Render a HTML spec for each component from its react implementation.
 require('babel-register');
 
 const {renderToString} = require('react-dom/server');
@@ -16,8 +17,18 @@ fs.readdir('components', (err, components) => {
   } else {
     components.forEach(component => {
       const componentPath = path.join('..', 'components', component, 'index.js')
-      const Component = require(componentPath).default;
-      const rendered = renderToString(React.createElement(Component, {}, null))
+
+      // By default, render the empty component. However, if the component exports a function named
+      // `spec`, use whatever it returns as the template to generate the specs.
+      const componentSpec = require(componentPath).spec;
+      let rendered;
+      if (componentSpec) {
+        rendered = renderToString(componentSpec())
+      } else {
+        // Render the empty component.
+        const Component = require(componentPath).default;
+        rendered = renderToString(React.createElement(Component, {}, null))
+      }
 
       const stream = fs.createWriteStream(path.join('components', component, 'spec.html'));
       stream.write(makePretty(rendered));
