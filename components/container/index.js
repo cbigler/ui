5a9754c36ccee6f6fcebe9dd618c35334ece7004
Ryan Gaus <rsg1egoman@gmail.com>
2017-06-20 14:12:03 -0400

    code: remove container
    Doesn't really make sense with the grid stuff in the place that it is.

--- components/container/index.js
+++
@@ -1,8 +0,0 @@
-import * as React from 'react';
-import classnames from 'classnames';
-
-export default function Container({children, fullWidth}) {
-  return <div className={classnames('container', {'container-full-width': fullWidth})}>
-    {children}
-  </div>;
-}
