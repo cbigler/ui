b461e9a968ededed6719d3e474d2a7367fe74fb2
Ryan Gaus <rsg1egoman@gmail.com>
2017-09-28 15:27:43 -0400

    chore: add disabled attributes to buttons

--- components/pager-button-group/index.js
+++ components/pager-button-group/index.js
@@ -3,15 +3,20 @@ import * as React from 'react';
 export default function PagerButtonGroup({
   showFirstLastButtons,
 
+  disabledPrevious,
+  disabledNext,
+  disabledStart,
+  disabledEnd,
+
   onClickPrevious,
   onClickNext,
   onClickStart,
   onClickEnd,
 }) {
   return <div className="pager-button-group">
-    {showFirstLastButtons ? <div className="pager-button" onClick={onClickStart}>&laquo;</div> : null}
-    <div className="pager-button" onClick={onClickNext}>&lsaquo;</div>
-    <div className="pager-button" onClick={onClickPrevious}>&rsaquo;</div>
-    {showFirstLastButtons ? <div className="pager-button" onClick={onClickEnd}>&raquo;</div> : null}
+    {showFirstLastButtons ? <div className={classnames('pager-button', {disabled: disabledEnd})} onClick={onClickEnd}>&laquo;</div> : null}
+    <div className={classnames('pager-button', {disabled: disabledPrevious})} onClick={onClickPrevious}>&lsaquo;</div>
+    <div className={classnames('pager-button', {disabled: disabledNext})} onClick={onClickNext}>&rsaquo;</div>
+    {showFirstLastButtons ? <div className={classnames('pager-button', {disabled: disabledStart})} onClick={onClickStart}>&raquo;</div> : null}
   </div>;
 }
