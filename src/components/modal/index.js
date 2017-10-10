02ee789c0a52bee83e0f5effc83274c7cd962f6d
Ryan Gaus <rsg1egoman@gmail.com>
2017-10-10 08:45:40 -0400

    code: reset modal styles to what they were before

--- src/components/modal/index.js
+++ src/components/modal/index.js
@@ -1,43 +1,23 @@
 import * as React from 'react';
 import classnames from 'classnames';
 
-export default class Modal extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = { shouldCenterModal: true };
+export default function Modal({
+  children,
 
-    this.resizedWindow = this.resizedWindow.bind(this);
-  }
-  componentDidMount() {
-    window.addEventListener('resize', this.resizedWindow);
-  }
-  resizedWindow() {
-    console.log('RESIZE', this.modal, this.modal.clientHeight > document.body.clientHeight)
-    this.setState({
-      shouldCenterModal: this.modal ? this.modal.clientHeight > document.body.clientHeight : true,
-    });
-  }
-  componentWillUnmount() {
-    window.removeEventListener('resize', this.resizedWindow);
-  }
-  render() {
-    return <div
-      className={classnames('modal-backdrop', {centered: this.state.shouldCenterModal})}
-      onClick={this.props.onClickBackdrop}
+  onClickBackdrop,
+  onClick,
+  onClose,
+}) {
+  return <div
+    className={classnames('modal-backdrop', {})}
+    onClick={onClickBackdrop}
+  >
+    <div
+      className="modal"
+      onClick={e => e.stopPropagation()}
     >
-      <div
-        className="modal"
-        onClick={e => e.stopPropagation()}
-        ref={ref => {
-          if (ref) {
-            console.log(ref);
-            this.modal = ref;
-          }
-        }}
-      >
-        {this.props.onClose ? <div className="modal-close" onClick={this.props.onClose}>&#10005;</div> : null}
-        {this.props.children}
-      </div>
-    </div>;
-  }
+      {onClose ? <div className="modal-close" onClick={onClose}>&#10005;</div> : null}
+      {children}
+    </div>
+  </div>;
 }
