20e71dabeba8be5dbdaa9b05814452adb426695f
Ryan Gaus <rsg1egoman@gmail.com>
2017-10-12 11:55:36 -0400

    code: make modals work on browsers that are short
    (modals horizontally scroll)

--- src/components/modal/dist/index.js
+++ src/components/modal/dist/index.js
@@ -1,32 +1,46 @@
+'use strict';
 
 Object.defineProperty(exports, "__esModule", {
   value: true
 });
 exports.default = Modal;
 
-var _react = require("react");
+var _react = require('react');
 
 var React = _interopRequireWildcard(_react);
+
+var _classnames = require('classnames');
+
+var _classnames2 = _interopRequireDefault(_classnames);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
 function _interopRequireWildcard(obj) { if (obj && obj.__esModule) { return obj; } else { var newObj = {}; if (obj != null) { for (var key in obj) { if (Object.prototype.hasOwnProperty.call(obj, key)) newObj[key] = obj[key]; } } newObj.default = obj; return newObj; } }
 
 function Modal(_ref) {
   var children = _ref.children,
-      onClose = _ref.onClose,
-      onClickBackdrop = _ref.onClickBackdrop;
+      onClickBackdrop = _ref.onClickBackdrop,
+      onClick = _ref.onClick,
+      onClose = _ref.onClose;
 
   return React.createElement(
-    "div",
-    { className: "modal-backdrop", onClick: onClickBackdrop },
+    'div',
+    {
+      className: (0, _classnames2.default)('modal-backdrop', {}),
+      onClick: onClickBackdrop
+    },
     React.createElement(
-      "div",
-      { className: "modal", onClick: function onClick(e) {
+      'div',
+      {
+        className: 'modal',
+        onClick: function onClick(e) {
           return e.stopPropagation();
-        } },
+        }
+      },
       onClose ? React.createElement(
-        "div",
-        { className: "modal-close", onClick: onClose },
-        "\u2715"
+        'div',
+        { className: 'modal-close', onClick: onClose },
+        '\u2715'
       ) : null,
       children
     )
--- src/components/modal/dist/story.js
+++ src/components/modal/dist/story.js
@@ -106,5 +106,110 @@ function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { de
       )
     )
   );
+}).addWithInfo('With height that overflows the viewport', function () {
+  return _react2.default.createElement(
+    'div',
+    null,
+    'foo',
+    _react2.default.createElement(
+      _index2.default,
+      { onClickBackdrop: (0, _addonActions.action)('backdrop click') },
+      _react2.default.createElement(
+        _card2.default,
+        { type: 'modal' },
+        _react2.default.createElement(
+          _card.CardHeader,
+          null,
+          'Confirmation'
+        ),
+        _react2.default.createElement(
+          _card.CardBody,
+          null,
+          _react2.default.createElement(
+            'p',
+            { style: { margin: 0 } },
+            'This token is for a thing. Lorem Ipsum is simply dummy text of the printing and typesetting industry and this is just here to make you believe in aliens.'
+          ),
+          _react2.default.createElement(
+            'p',
+            { style: { margin: 0 } },
+            'This token is for a thing. Lorem Ipsum is simply dummy text of the printing and typesetting industry and this is just here to make you believe in aliens.'
+          ),
+          _react2.default.createElement(
+            'p',
+            { style: { margin: 0 } },
+            'This token is for a thing. Lorem Ipsum is simply dummy text of the printing and typesetting industry and this is just here to make you believe in aliens.'
+          ),
+          _react2.default.createElement(
+            'p',
+            { style: { margin: 0 } },
+            'This token is for a thing. Lorem Ipsum is simply dummy text of the printing and typesetting industry and this is just here to make you believe in aliens.'
+          ),
+          _react2.default.createElement(
+            'p',
+            { style: { margin: 0 } },
+            'This token is for a thing. Lorem Ipsum is simply dummy text of the printing and typesetting industry and this is just here to make you believe in aliens.'
+          ),
+          _react2.default.createElement(
+            'p',
+            { style: { margin: 0 } },
+            'This token is for a thing. Lorem Ipsum is simply dummy text of the printing and typesetting industry and this is just here to make you believe in aliens.'
+          ),
+          _react2.default.createElement(
+            'p',
+            { style: { margin: 0 } },
+            'This token is for a thing. Lorem Ipsum is simply dummy text of the printing and typesetting industry and this is just here to make you believe in aliens.'
+          ),
+          _react2.default.createElement(
+            'p',
+            { style: { margin: 0 } },
+            'This token is for a thing. Lorem Ipsum is simply dummy text of the printing and typesetting industry and this is just here to make you believe in aliens.'
+          ),
+          _react2.default.createElement(
+            'p',
+            { style: { margin: 0 } },
+            'This token is for a thing. Lorem Ipsum is simply dummy text of the printing and typesetting industry and this is just here to make you believe in aliens.'
+          ),
+          _react2.default.createElement(
+            'p',
+            { style: { margin: 0 } },
+            'This token is for a thing. Lorem Ipsum is simply dummy text of the printing and typesetting industry and this is just here to make you believe in aliens.'
+          ),
+          _react2.default.createElement(
+            'p',
+            { style: { margin: 0 } },
+            'This token is for a thing. Lorem Ipsum is simply dummy text of the printing and typesetting industry and this is just here to make you believe in aliens.'
+          ),
+          _react2.default.createElement(
+            'p',
+            { style: { margin: 0 } },
+            'This token is for a thing. Lorem Ipsum is simply dummy text of the printing and typesetting industry and this is just here to make you believe in aliens.'
+          ),
+          _react2.default.createElement(
+            'p',
+            { style: { margin: 0 } },
+            'This token is for a thing. Lorem Ipsum is simply dummy text of the printing and typesetting industry and this is just here to make you believe in aliens.'
+          ),
+          _react2.default.createElement(
+            'p',
+            { style: { margin: 0 } },
+            'This token is for a thing. Lorem Ipsum is simply dummy text of the printing and typesetting industry and this is just here to make you believe in aliens.'
+          ),
+          _react2.default.createElement('br', null),
+          _react2.default.createElement(
+            _index4.default,
+            null,
+            'Yes'
+          ),
+          _react2.default.createElement('br', null),
+          _react2.default.createElement(
+            _index4.default,
+            null,
+            'No'
+          )
+        )
+      )
+    )
+  );
 });
 
