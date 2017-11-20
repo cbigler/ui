335beb15d939a6ab9a5ccfe36fc3b83459978899
Ryan Gaus <rsg1egoman@gmail.com>
2017-11-20 13:41:42 -0500

    chore: rebuild

--- src/components/navbar/dist/index.js
+++ src/components/navbar/dist/index.js
@@ -10,6 +10,12 @@ var _react = require('react');
 
 var React = _interopRequireWildcard(_react);
 
+var _classnames = require('classnames');
+
+var _classnames2 = _interopRequireDefault(_classnames);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
 function _interopRequireWildcard(obj) { if (obj && obj.__esModule) { return obj; } else { var newObj = {}; if (obj != null) { for (var key in obj) { if (Object.prototype.hasOwnProperty.call(obj, key)) newObj[key] = obj[key]; } } newObj.default = obj; return newObj; } }
 
 function Navbar(_ref) {
@@ -58,12 +64,22 @@ function NavbarItem(_ref2) {
   var activePage = _ref2.activePage,
       pageName = _ref2.pageName,
       href = _ref2.href,
-      children = _ref2.children;
+      children = _ref2.children,
+      locked = _ref2.locked;
 
   return React.createElement(
     'li',
-    { className: pageName.indexOf(activePage) >= 0 ? 'navbar-item-active' : 'navbar-item' },
-    React.createElement(
+    {
+      className: (0, _classnames2.default)('navbar-item', {
+        'navbar-item-active': pageName.indexOf(activePage) >= 0,
+        'navbar-item-locked': locked
+      })
+    },
+    locked ? React.createElement(
+      'span',
+      null,
+      children
+    ) : React.createElement(
       'a',
       { href: href },
       children
