48d68a09d358d16f8555ac2320a2c1f6d53661dc
Ryan Gaus <rsg1egoman@gmail.com>
2018-07-02 09:19:28 -0400

    icon: add floorplan icon

--- src/components/icons/index.js
+++ src/components/icons/index.js
@@ -837,6 +837,20 @@ const ICONS = {
           </g>
       </g>
   </svg>,
+  IconFloorplans: ({color, width, height}) => <svg width={width || 20} height={height || 18} viewBox='0 0 20 18' xmlns='http://www.w3.org/2000/svg'>
+    <g id='Page-1' fill='none' fillRule='evenodd'>
+        <g id='IconFloorplans' transform='translate(0 -1)'>
+            <rect id='bounds' fillOpacity='0' fill={color} width='20' height='20'
+            />
+            <polygon id='Stroke-1' stroke={color} strokeWidth='1.5' strokeLinecap='square'
+            strokeLinejoin='round' points='1 18 13 18 13 8 1 8' />
+            <polyline id='Stroke-1-Copy' stroke={color} strokeWidth='1.5' strokeLinecap='square'
+            strokeLinejoin='round' points='13 15 16 15 16 5 4 5 4 8' />
+            <polyline id='Stroke-1-Copy-2' stroke={color} strokeWidth='1.5' strokeLinecap='square'
+            strokeLinejoin='round' points='16 12 19 12 19 2 7 2 7 5' />
+        </g>
+    </g>
+</svg>
 };
 
 // Wrap each icon in a component.
