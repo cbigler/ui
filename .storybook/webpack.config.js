b6342afbd72e18fc8ceeec4a3efbd05d2f97353b
Ryan Gaus <rsg1egoman@gmail.com>
2018-09-21 16:25:33 -0400

    code: add css modules loader to storybook configuration

--- .storybook/webpack.config.js
+++ .storybook/webpack.config.js
@@ -1,24 +1,16 @@
 const path = require('path');
-
-// module.exports = {
-//   module: {
-//     rules: [
-//       {
-//         test: /\.scss$/,
-//         loaders: ["style-loader", "css-loader", "sass-loader"],
-//         include: path.resolve(__dirname, '../')
-//       }
-//     ]
-//   }
-// }
-
-
 const jsonImporter = require('@density/node-sass-json-importer');
 
-// Webpack config
 module.exports = {
   module: {
     rules: [
+      // *.scss files are processed through three steps:
+      // - style-loader, which includes them in the output bundle and will include a style tag in
+      //   the dom when the component's compiled bundle is loaded.
+      // - css-loader, which does the css modules transformations - this means each class name in
+      //   each file is rewritten so it only targets the elements that are desired.
+      // - sass-loader, which does the sass compilation with node-sass. In addition, this uses
+      //   node-sass-json importer to permit importing of json files containing variables.
       {
         test: /\.scss$/,
         use: [
@@ -26,7 +18,10 @@ module.exports = {
           {
             loader: 'css-loader',
             options: {
-              importLoaders: 1
+              importLoaders: 1,
+              modules: true,
+              /* for example, dui-styles-button-1SZwRzu8 */
+              localIdentName: 'dui-[name]-[local]-[hash:base64:8]',
             },
           },
           {
