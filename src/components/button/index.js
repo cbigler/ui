0c1599cbeba6e4a86fc32237fb708b55f0dd5c95
Ryan Gaus <rsg1egoman@gmail.com>
2018-09-21 16:58:22 -0400

    code: ported button component to use css modules

--- src/components/button/index.js
+++ src/components/button/index.js
@@ -1,12 +1,19 @@
-import * as React from 'react';
+import React from 'react';
 import classnames from 'classnames';
+import camelcase from 'camelcase';
+
+import styles from './styles.scss';
 
 export default function Button(props) {
   const { className, size, children } = props;
-  const restProps = Object.assign({}, props, {className: undefined, size: undefined, children: undefined});
+  const restProps = Object.assign({}, props, {
+    className: undefined,
+    size: undefined,
+    children: undefined,
+  });
+
   return <button {...restProps} className={classnames(
-    'button',
-    size ? `button-${size}` : null,
-    className
+    styles.button,
+    size ? styles[camelcase(`button-${size}`)] : null,
   )}>{children}</button>;
 }
