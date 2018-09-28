c86d6b912f3ea8b29b398cd603bf51dc906a714f
Ryan Gaus <rsg1egoman@gmail.com>
2018-09-28 13:28:47 -0400

    code: migrate percentage bar to css modules

--- src/components/percentage-bar/index.js
+++ src/components/percentage-bar/index.js
@@ -1,10 +1,12 @@
-import * as React from 'react';
+import React from 'react';
 import classnames from 'classnames';
 
 // By default, hide the percentage bar below screen widths of this width.
 const PERCENTAGE_BAR_DEFAULT_BREAK_WIDTH = 768;
 
 const MDASH = String.fromCharCode(8212);
+
+import styles from './styles.scss';
 
 export default class PercentageBar extends React.Component {
   componentDidMount() {
@@ -34,15 +36,18 @@ export default class PercentageBar extends React.Component {
     // Check if the percentage bar has enough screen width to render the bar or not.
     const isExpanded = document.body && document.body.clientWidth >= breakWidth;
 
-    return <div className="percentage-bar-container">
+    return <div className={styles.percentageBarContainer}>
       <div
-        className="percentage-bar"
+        className={styles.percentageBar}
         style={{display: isExpanded ? 'inline-block' : 'none'}} /* Is the bar visible? */
       >
-        <div className="percentage-bar-colored-section" style={{width: `${percentage * 100}%`}} />
+        <div className={styles.percentageBarColoredSection} style={{width: `${percentage * 100}%`}} />
       </div>
       <span
-        className={classnames("percentage-bar-text", formattedPercentage === null ? 'disabled' : null)}
+        className={classnames(
+          styles.percentageBarText,
+          formattedPercentage === null ?  styles.percentageBarTextDisabled : null,
+        )}
 
         // If the text is preceded by a bar (ie, isExpanded is true), then style it differently.
         style={{
