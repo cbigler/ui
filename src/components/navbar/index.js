2a24a8918ec1f5c9ad330bb68a04b9871c18681e
Ryan Gaus <rsg1egoman@gmail.com>
2018-10-03 12:19:05 -0400

    code: use density svg logo in navbar

--- src/components/navbar/index.js
+++ src/components/navbar/index.js
@@ -1,5 +1,8 @@
-import * as React from 'react';
+import React from 'react';
 import classnames from 'classnames';
+
+import { IconLock } from '@density/ui-icons';
+import { DensityLogo } from '@density/ui-density-mark';
 
 export default class Navbar extends React.Component {
   constructor(props) {
@@ -34,10 +37,7 @@ export default class Navbar extends React.Component {
           /> : null}
 
           {/* Render the logo */}
-          <img
-            src="http://style-guide.density.io/assets/images/app_bar_logo.png"
-            alt="Density Logo"
-          />
+          <DensityLogo size={20} />
         </div>
         {subtitle ? <div className="navbar-brand-subtitle">{subtitle}</div> : null}
         <div className="navbar-items">{children}</div>
