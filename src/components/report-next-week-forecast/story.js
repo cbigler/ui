4c1fbdc9abf21d9c89b9c0b33fcad19c5bb1e8ee
Ryan Gaus <rsg1egoman@gmail.com>
2018-10-17 11:33:06 -0400

    code: wrap next week forecast in a div with padding

--- src/components/report-next-week-forecast/story.js
+++ src/components/report-next-week-forecast/story.js
@@ -8,49 +8,51 @@ import ReportNextWeekForecast, { DAYS } from './';
 
 storiesOf('ReportNextWeekForecast', module)
   .addWithInfo('Default view', () => (
-    <ReportNextWeekForecast
-      title="Next week's visits"
-      startDate={moment("2018-03-14T00:00:00-04:00")}
-      endDate={moment("2018-03-18T00:00:00-04:00")}
-      spaces={["Cafe Bruno"]}
-      busiestDay={DAYS.FRIDAY}
-      forecasts={[
-        {
-          visits: 300,
-          high: 20,
-          low: 20,
-          stdDev: 20,
-        },
-        {
-          visits: 622,
-          high: 40,
-          low: 40,
-          stdDev: 40,
-        },
-        {
-          visits: 412,
-          high: 60,
-          low: 60,
-          stdDev: 60,
-        },
-        {
-          visits: 614,
-          high: 10,
-          low: 10,
-          stdDev: 10,
-        },
-        {
-          visits: 895,
-          high: 60,
-          low: 60,
-          stdDev: 60,
-        },
-        {
-          visits: 622,
-          high: 40,
-          low: 40,
-          stdDev: 40,
-        },
-      ]}
-    />
+    <div style={{width: '100%',paddingTop: 100}}>
+      <ReportNextWeekForecast
+        title="Next week's visits"
+        startDate={moment("2018-03-14T00:00:00-04:00")}
+        endDate={moment("2018-03-18T00:00:00-04:00")}
+        spaces={["Cafe Bruno"]}
+        busiestDay={DAYS.FRIDAY}
+        forecasts={[
+          {
+            visits: 300,
+            high: 20,
+            low: 20,
+            stdDev: 20,
+          },
+          {
+            visits: 622,
+            high: 40,
+            low: 40,
+            stdDev: 40,
+          },
+          {
+            visits: 412,
+            high: 60,
+            low: 60,
+            stdDev: 60,
+          },
+          {
+            visits: 614,
+            high: 10,
+            low: 10,
+            stdDev: 10,
+          },
+          {
+            visits: 895,
+            high: 60,
+            low: 60,
+            stdDev: 60,
+          },
+          {
+            visits: 622,
+            high: 40,
+            low: 40,
+            stdDev: 40,
+          },
+        ]}
+      />
+    </div>
   ))
