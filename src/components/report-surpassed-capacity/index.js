5ceec4e61e522bc1cacc62353acf15aa48717c48
Ryan Gaus <rsg1egoman@gmail.com>
2018-10-30 13:40:42 -0400

    code: add bucket joining together for surpassed capacity chart
    This required so that the metric that indicates how long the count went
    over capacity will be correct even if that time it was over capacity was
    split between multiple buckets.

--- src/components/report-surpassed-capacity/index.js
+++ src/components/report-surpassed-capacity/index.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import propTypes from 'prop-types';
 import moment from 'moment';
 import * as d3Scale from 'd3-scale'; /* note: this package doesn't have a `default` export */
 
@@ -19,6 +20,18 @@ function getMaximumsByKey(data, keyFunction) {
   return data.filter((_, index) => {
     return keyedData[index] === maxValue;
   });
+}
+
+function calculateColorForBucket(count, capacity, {quietBusyThreshold, busyOverCapacityThreshold}) {
+  const percent = count / capacity;
+
+  if (0 <= percent && percent < quietBusyThreshold) {
+    return QUIET_COLOR;
+  } else if (quietBusyThreshold <= percent && percent < busyOverCapacityThreshold) {
+    return BUSY_COLOR;
+  } else {
+    return OVER_CAPACITY_COLOR;
+  }
 }
 
 class SurpassedCapacityChart extends Component {
@@ -60,10 +73,10 @@ class SurpassedCapacityChart extends Component {
     const end = moment.duration(timeSegment.end);
 
     const marks = [];
-    for (let i = start; i.asHours() <= end.asHours(); i = i.clone().add(1, 'hour')) {
-      let label = i.asHours();
+    for (let i = start; i.hours() <= end.hours(); i = i.clone().add(1, 'hour')) {
+      let label = i.hours();
       marks.push({
-        value: i.asSeconds(),
+        value: label * 3600, // convert label to seconds
         label: moment.utc().startOf('day').add(label, 'hours').format('ha').slice(0, -1),
       });
     }
@@ -72,8 +85,26 @@ class SurpassedCapacityChart extends Component {
   }
 
   render() {
-    const { data, startDate, endDate, timeSegment, highestCapacity, capacity } = this.props;
+    const {
+      data,
+      startDate,
+      endDate,
+      timeSegment,
+
+      highestCapacity,
+      capacity,
+
+      quietBusyThreshold,
+      busyOverCapacityThreshold,
+    } = this.props;
     const { width } = this.state;
+
+    if (quietBusyThreshold > 1) {
+      throw new Error('quietBusyThreshold is greater than one, it must be within 0...1');
+    }
+    if (busyOverCapacityThreshold > 1) {
+      throw new Error('busyOverCapacityThreshold is greater than one, it must be within 0...1');
+    }
 
     const xScale = d3Scale.scaleLinear()
       .domain([
@@ -143,17 +174,13 @@ class SurpassedCapacityChart extends Component {
                     const xPos = xScale(this.convertTimeToSeconds(start));
                     const percent = count / capacity;
 
-                    let color;
-                    if (0 <= percent && percent < 0.39) {
-                      color = QUIET_COLOR;
-                    } else if (0.40 <= percent && percent < 0.59) {
-                      color = BUSY_COLOR;
-                    } else {
-                      color = OVER_CAPACITY_COLOR;
-                    }
+                    let color = calculateColorForBucket(
+                      count, capacity,
+                      {quietBusyThreshold, busyOverCapacityThreshold},
+                    );
 
                     const highestCapacityLabel = highestCapacity.find(h => (
-                      h.start === start && h.end === end
+                      h.day.format('ddd') === day && h.start === start && h.end === end
                     ));
 
                     const width = xScale(this.convertTimeToSeconds(end)) - xPos;
@@ -196,17 +223,70 @@ export default function ReportSurpassedCapacity({
   timeSegment,
   data,
   capacity,
+  quietBusyThreshold,
+  busyOverCapacityThreshold,
 }) {
-  const highestCapacityPerDay = data.reduce((acc, day, index) => {
+
+  const highestCapacityPerDay = data.reduce((acc, day, dayIndex) => {
+    // Add indexes to days so that the buckets can be merged later on to figure out for how much
+    // time we were over capacity.
+    const daysWithIndexes = day.map((d, bucketIndex) => Object.assign({}, d, {dayIndex, bucketIndex}));
     return [
       ...acc,
-      ...getMaximumsByKey(day, d => d.count / capacity).map(i => Object.assign({}, i, {
-        day: startDate.clone().add(index, 'days'),
-      }))
+      ...getMaximumsByKey(daysWithIndexes, d => d.count / capacity).map(i => Object.assign({}, i, {
+        day: startDate.clone().add(dayIndex, 'days'),
+      })),
     ];
   }, []);
 
   const highestCapacity = getMaximumsByKey(highestCapacityPerDay, d => d.count / capacity);
+
+  // THere's a distinct possibility that we found a region that has more over capacity regions
+  // before it. In order to show an accurate "over capacity length", move balkwards through the list
+  // of regions until we find one that is no longer "over capacity".
+  const highCapacityRegionBounds = highestCapacity.map(highCapacityBucket => {
+    const originalBucketColor = calculateColorForBucket(
+      highCapacityBucket.count, capacity,
+      {quietBusyThreshold, busyOverCapacityThreshold},
+    );
+
+    const dayDataArray = data[highCapacityBucket.dayIndex];
+
+    // Find the start of this region
+    let bucketIndex = highCapacityBucket.bucketIndex;
+    let bucketColor = originalBucketColor;
+
+    // Keep going backwards until the bucket we've run into is no longer the same type as the original one.
+    while (bucketIndex > 0 && bucketColor === originalBucketColor) {
+      bucketIndex -= 1;
+      const bucketCount = dayDataArray[bucketIndex].count;
+      bucketColor = calculateColorForBucket(
+        bucketCount, capacity,
+        {quietBusyThreshold, busyOverCapacityThreshold},
+      );
+    }
+    const startOfRegion = dayDataArray[bucketIndex+1].start;
+
+    // Find the end of this region
+    bucketIndex = highCapacityBucket.bucketIndex;
+    bucketColor = originalBucketColor;
+
+    // Keep going forwards until the bucket we've run into is no longer the same type as the original one.
+    while (bucketIndex < dayDataArray.length && bucketColor === originalBucketColor) {
+      bucketIndex += 1;
+      const bucketCount = dayDataArray[bucketIndex].count;
+      bucketColor = calculateColorForBucket(
+        bucketCount, capacity,
+        {quietBusyThreshold, busyOverCapacityThreshold},
+      );
+    }
+    const endOfRegion = dayDataArray[bucketIndex-1].end;
+
+    return Object.assign({}, highCapacityBucket, {
+      startOfRegion,
+      endOfRegion,
+    });
+  });
 
   return (
     <ReportWrapper
@@ -216,24 +296,44 @@ export default function ReportSurpassedCapacity({
       spaces={spaces}
     >
       <ReportSubHeader
-        title={highestCapacity.map(highCapacityDay => (
+        title={highCapacityRegionBounds.map(highCapacityDay => (
           <span className={styles.header} key={`${highCapacityDay.start},${highCapacityDay.end}`}>
             <strong>{highCapacityDay.day.format('dddd')}</strong> was over
             capacity for <strong>
               {moment.duration(
-                moment.duration(highCapacityDay.end).asMilliseconds() -
-                moment.duration(highCapacityDay.start).asMilliseconds()
+                moment.duration(highCapacityDay.endOfRegion).asMilliseconds() -
+                moment.duration(highCapacityDay.startOfRegion).asMilliseconds()
               ).humanize()}
             </strong>, peaking at{' '}
-            <strong>{highCapacityDay.count}</strong> visitors.
+            <strong>{highCapacityDay.count}</strong> visitors around <strong>
+              {
+                moment.utc()
+                  .startOf('day')
+                  .add(moment.duration(highCapacityDay.start))
+                  .format('H:mma')
+                  .slice(0, -1)
+              }
+            </strong>.
           </span>
         ))}
       >
         <ReportOptionBar
           options={[
-            {id: 0, label: 'Quiet (0-39)', color: QUIET_COLOR},
-            {id: 1, label: 'Busy (40-59)', color: BUSY_COLOR},
-            {id: 2, label: 'Over Capacity (60+)', color: OVER_CAPACITY_COLOR},
+            {
+              id: 0,
+              label: `Quiet (0-${Math.round(quietBusyThreshold*100)-1})`,
+              color: QUIET_COLOR,
+            },
+            {
+              id: 1,
+              label: `Busy (${Math.round(quietBusyThreshold*100)}-${Math.round(busyOverCapacityThreshold*100)-1})`,
+              color: BUSY_COLOR,
+            },
+            {
+              id: 2,
+              label: `Over Capacity (${Math.round(busyOverCapacityThreshold*100)}+)`,
+              color: OVER_CAPACITY_COLOR,
+            },
           ]}
         />
       </ReportSubHeader>
@@ -242,11 +342,45 @@ export default function ReportSurpassedCapacity({
           data={data}
           startDate={startDate}
           endDate={endDate}
+          timeSegment={timeSegment}
+
           capacity={capacity}
           highestCapacity={highestCapacity}
-          timeSegment={timeSegment}
+
+          quietBusyThreshold={quietBusyThreshold}
+          busyOverCapacityThreshold={busyOverCapacityThreshold}
         />
       </ReportCard>
     </ReportWrapper>
   )
 }
+
+ReportSurpassedCapacity.propTypes = {
+  title: propTypes.string.isRequired,
+  startDate: propTypes.instanceOf(moment).isRequired,
+  endDate: propTypes.instanceOf(moment).isRequired,
+  spaces: propTypes.arrayOf(propTypes.string).isRequired,
+
+  timeSegment: propTypes.shape({
+    id: propTypes.string.isRequired,
+    name: propTypes.string.isRequired,
+    start: propTypes.string.isRequired,
+    end: propTypes.string.isRequired,
+    days: propTypes.arrayOf(propTypes.string).isRequired,
+    spaces: propTypes.arrayOf(propTypes.any).isRequired,
+  }).isRequired,
+  capacity: propTypes.number.isRequired,
+
+  quietBusyThreshold: propTypes.number.isRequired,
+  busyOverCapacityThreshold: propTypes.number.isRequired,
+
+  data: propTypes.arrayOf(
+    propTypes.arrayOf(
+      propTypes.shape({
+        count: propTypes.number.isRequired,
+        start: propTypes.string.isRequired,
+        end: propTypes.string.isRequired,
+      }).isRequired,
+    ).isRequired,
+  ).isRequired,
+};
