57649cde040ba12b2070ce6356f15f5af1a9fe7f
Gus Cost <guscost@gmail.com>
2018-11-08 15:55:38 -0500

    fix symlink direction part 2

---
+++ helpers/text.js
@@ -0,0 +1,21 @@
+
+// This takes a list of strings, React elements, or anything, and intersperses in
+// correct (Oxford-comma'd) english separators, e.g. "First, Second, and Third"
+function toEnglishList(items) {
+  if (items.length < 2) { 
+    return items;
+  } else if (items.length === 2) {
+    return [items[0], ' and ', items[1]];
+  } else {
+    return items.reduce(function (acc, item, index) {
+      return acc.concat([
+        index < (items.length - 1) ? (acc.length ? ', ' : '') : ', and ',
+        item
+      ]);
+    }, []);
+  }
+}
+
+module.exports = {
+  toEnglishList: toEnglishList
+};
