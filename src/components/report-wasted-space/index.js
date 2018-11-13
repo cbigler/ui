6a9b30ab42a9175964c236f26f0de03cdac26ca1
Ryan Gaus <rsg1egoman@gmail.com>
2018-11-13 14:48:33 -0500

    code: finish up wasted space report
    - Add small text above bars when they collapse below 50px
      - Maybe we should make this more dynamic, but I'm fine with this at
      the moment.
    - Make props functional

--- src/components/report-wasted-space/index.js
+++ src/components/report-wasted-space/index.js
@@ -6,6 +6,22 @@ import styles from './styles.scss';
 import ReportWrapper, { ReportSubHeader, ReportCard } from '@density/ui-report-wrapper';
 
 export default class ReportWastedSpace extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { width: null };
+  }
+  componentDidMount() {
+    this.onResize = this.onResize.bind(this);
+    window.addEventListener('resize', this.onResize);
+    this.onResize();
+  }
+  componentWillUnmount() {
+    window.removeEventListener('resize', this.onResize);
+  }
+  onResize() {
+    this.setState({width: this.utilizationBar.clientWidth});
+  }
+
   render() {
     const {
       title,
@@ -20,6 +36,7 @@ export default class ReportWastedSpace extends Component {
       normalPercent,
       overutilizedPercent,
     } = this.props;
+    const { width } = this.state;
     return (
       <ReportWrapper
         title={title}
@@ -28,24 +45,51 @@ export default class ReportWastedSpace extends Component {
         spaces={spaces}
       >
         <ReportSubHeader
-          title={<span><strong>30%</strong> of your spaces are <strong>underutilized</strong>.</span>}
+          title={<span><strong>{underutilizedPercent}%</strong> of your spaces are <strong>underutilized</strong>.</span>}
         />
         <ReportCard>
-          <div className={styles.utilizationBar}>
-            <div className={classnames(styles.region, styles.under)} style={{width: '30%'}}>
-              30%
+          <div className={styles.utilizationBar} ref={r => { this.utilizationBar = r; }}>
+            <div
+              className={classnames(
+                styles.region,
+                styles.under,
+                {[styles.textAbove]: width * (underutilizedPercent / 100) < 30}
+              )}
+              style={{width: `${underutilizedPercent}%`}}
+            >
+              <span className={styles.label}>{underutilizedPercent}%</span>
             </div>
-            <div className={classnames(styles.region, styles.normal)} style={{width: '51%'}}>
-              51%
+            <div
+              className={classnames(
+                styles.region,
+                styles.normal,
+                {[styles.textAbove]: width * (normalPercent / 100) < 30}
+              )}
+              style={{width: `${normalPercent}%`}}
+            >
+              <span className={styles.label}>{normalPercent}%</span>
             </div>
-            <div className={classnames(styles.region, styles.over)} style={{width: '19%'}}>
-              19%
+            <div
+              className={classnames(
+                styles.region,
+                styles.over,
+                {[styles.textAbove]: width * (overutilizedPercent / 100) < 30}
+              )}
+              style={{width: `${overutilizedPercent}%`}}
+            >
+              <span className={styles.label}>{overutilizedPercent}%</span>
             </div>
           </div>
           <ul className={styles.utilizationLabels}>
-            <li className={styles.under}>Underutilized (0-20% utilization)</li>
-            <li className={styles.normal}>Normal (20-80% utilization)</li>
-            <li className={styles.over}>Overutilized (80-100% utilization)</li>
+            <li className={styles.under}>
+              Underutilized (0%-{underutilizedNormalThreshold*100}% utilization)
+            </li>
+            <li className={styles.normal}>
+              Normal ({underutilizedNormalThreshold*100}%-{normalOverutilizedThreshold*100}% utilization)
+            </li>
+            <li className={styles.over}>
+              Overutilized ({normalOverutilizedThreshold*100}%-100% utilization)
+            </li>
           </ul>
         </ReportCard>
       </ReportWrapper>
--- src/components/report-wasted-space/story.js
+++ src/components/report-wasted-space/story.js
@@ -4,14 +4,48 @@ import { action } from '@storybook/addon-actions';
 
 import moment from 'moment';
 
-import ReportWastedSpace from './';
+import ReportWastedSpace from './index';
 
 storiesOf('ReportWastedSpace', module)
-  .addWithInfo('With a text body', () => (
+  .addWithInfo('Default', () => (
     <ReportWastedSpace
       title="Surpassed capacity example"
       startDate={moment("2018-10-21T07:00:00.000Z")}
       endDate={moment("2018-10-28T06:59:59.999Z")}
       spaces={["Cafe Ole"]}
+
+      underutilizedNormalThreshold={0.2}
+      normalOverutilizedThreshold={0.8}
+      underutilizedPercent={30}
+      normalPercent={51}
+      overutilizedPercent={19}
+    />
+  ))
+  .addWithInfo('With a final bar that is tiny on mobile', () => (
+    <ReportWastedSpace
+      title="Surpassed capacity example"
+      startDate={moment("2018-10-21T07:00:00.000Z")}
+      endDate={moment("2018-10-28T06:59:59.999Z")}
+      spaces={["Cafe Ole"]}
+
+      underutilizedNormalThreshold={0.2}
+      normalOverutilizedThreshold={0.8}
+      underutilizedPercent={30}
+      normalPercent={68}
+      overutilizedPercent={5}
+    />
+  ))
+  .addWithInfo('With a tiny final bar', () => (
+    <ReportWastedSpace
+      title="Surpassed capacity example"
+      startDate={moment("2018-10-21T07:00:00.000Z")}
+      endDate={moment("2018-10-28T06:59:59.999Z")}
+      spaces={["Cafe Ole"]}
+
+      underutilizedNormalThreshold={0.2}
+      normalOverutilizedThreshold={0.8}
+      underutilizedPercent={30}
+      normalPercent={68}
+      overutilizedPercent={2}
     />
   ))
