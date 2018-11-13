da73a38659cfe8d5c3273c2f190e630bdab6a336
Ryan Gaus <rsg1egoman@gmail.com>
2018-11-13 15:13:49 -0500

    code: add spacing above wasted space report in stories

--- src/components/report-wasted-space/story.js
+++ src/components/report-wasted-space/story.js
@@ -8,44 +8,50 @@ import ReportWastedSpace from './index';
 
 storiesOf('ReportWastedSpace', module)
   .addWithInfo('Default', () => (
-    <ReportWastedSpace
-      title="Surpassed capacity example"
-      startDate={moment("2018-10-21T07:00:00.000Z")}
-      endDate={moment("2018-10-28T06:59:59.999Z")}
-      spaces={["Cafe Ole"]}
+    <div style={{width: '100%', marginTop: 100}}>
+      <ReportWastedSpace
+        title="Surpassed capacity example"
+        startDate={moment("2018-10-21T07:00:00.000Z")}
+        endDate={moment("2018-10-28T06:59:59.999Z")}
+        spaces={["Cafe Ole"]}
 
-      underutilizedNormalThreshold={0.2}
-      normalOverutilizedThreshold={0.8}
-      underutilizedPercent={30}
-      normalPercent={51}
-      overutilizedPercent={19}
-    />
+        underutilizedNormalThreshold={0.2}
+        normalOverutilizedThreshold={0.8}
+        underutilizedPercent={30}
+        normalPercent={51}
+        overutilizedPercent={19}
+      />
+    </div>
   ))
   .addWithInfo('With a final bar that is tiny on mobile', () => (
-    <ReportWastedSpace
-      title="Surpassed capacity example"
-      startDate={moment("2018-10-21T07:00:00.000Z")}
-      endDate={moment("2018-10-28T06:59:59.999Z")}
-      spaces={["Cafe Ole"]}
+    <div style={{width: '100%', marginTop: 100}}>
+      <ReportWastedSpace
+        title="Surpassed capacity example"
+        startDate={moment("2018-10-21T07:00:00.000Z")}
+        endDate={moment("2018-10-28T06:59:59.999Z")}
+        spaces={["Cafe Ole"]}
 
-      underutilizedNormalThreshold={0.2}
-      normalOverutilizedThreshold={0.8}
-      underutilizedPercent={30}
-      normalPercent={68}
-      overutilizedPercent={5}
-    />
+        underutilizedNormalThreshold={0.2}
+        normalOverutilizedThreshold={0.8}
+        underutilizedPercent={30}
+        normalPercent={68}
+        overutilizedPercent={5}
+      />
+    </div>
   ))
   .addWithInfo('With a tiny final bar', () => (
-    <ReportWastedSpace
-      title="Surpassed capacity example"
-      startDate={moment("2018-10-21T07:00:00.000Z")}
-      endDate={moment("2018-10-28T06:59:59.999Z")}
-      spaces={["Cafe Ole"]}
+    <div style={{width: '100%', marginTop: 100}}>
+      <ReportWastedSpace
+        title="Surpassed capacity example"
+        startDate={moment("2018-10-21T07:00:00.000Z")}
+        endDate={moment("2018-10-28T06:59:59.999Z")}
+        spaces={["Cafe Ole"]}
 
-      underutilizedNormalThreshold={0.2}
-      normalOverutilizedThreshold={0.8}
-      underutilizedPercent={30}
-      normalPercent={68}
-      overutilizedPercent={2}
-    />
+        underutilizedNormalThreshold={0.2}
+        normalOverutilizedThreshold={0.8}
+        underutilizedPercent={30}
+        normalPercent={68}
+        overutilizedPercent={2}
+      />
+    </div>
   ))
