5d796f5524ba8e8198e2f8ca3f7951ef4214fc11
Gus Cost <guscost@gmail.com>
2019-02-13 10:20:59 -0500

    code: webpack and npm package

--- src/index.js
+++ src/index.js
@@ -1,1 +1,36 @@
-export default {}
+import './fonts';
+
+export { default as AppBar } from './app-bar';
+export { default as AppFrame } from './app-frame';
+export { default as AppPane } from './app-pane';
+export { default as AppScrollView } from './app-scroll-view';
+export { default as AppSidebar } from './app-sidebar';
+export { default as Button } from './button';
+export { default as DashboardReportGrid } from './dashboard-report-grid';
+export { default as DatePicker } from './date-picker';
+export { default as DateRangePicker } from './date-range-picker';
+export { default as DensityMark } from './density-mark';
+export { default as Floorplan } from './floorplan';
+export { default as Icons } from './icons';
+export { default as InfoPopup } from './info-popup';
+export { default as InputBox } from './input-box';
+export { default as Modal } from './modal';
+export { default as PagerButtonGroup } from './pager-button-group';
+export { default as PercentageBar } from './percentage-bar';
+export { default as RadioButton } from './radio-button';
+export { default as Switch } from './switch';
+export { default as Toast } from './toast';
+
+export {
+  InputStackGroup,
+  InputStackItem
+} from './input-stack';
+
+export {
+  default as Card,
+  CardBody,
+  CardLoading,
+  CardTable,
+  CardWell,
+  CardWellHighlight
+} from './card';
---
+++ webpack.config.js
@@ -0,0 +1,105 @@
+const fs = require('fs');
+const path = require('path');
+const jsonImporter = require('@density/node-sass-json-importer');
+const camelcase = require('camelcase');
+
+const package = require('./package.json');
+
+// External dict of peer dependencies so they can be loaded in dynamically.
+const peerDependencies = {};
+for (const key in package.peerDependencies) {
+  peerDependencies[key] = {
+    root: key,
+    commonjs2: key,
+    commonjs: key,
+    amd: key,
+  };
+}
+
+module.exports = {
+  entry: path.join(path.resolve(__dirname), 'src', 'index.js'),
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'index.js',
+    library: '@density/ui',
+    libraryTarget: 'umd',
+  },
+  externals: [
+    peerDependencies,
+  ],
+
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        // skip all of node_modules EXCEPT for modules > ES5
+        exclude: /node_modules\/(?![camelcase])/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: [
+              // compile jsx => React.createElement
+              '@babel/preset-react',
+              // use "modern" javascript, whatever that means
+              ['@babel/preset-env', {
+                // provide polyfills as necessary
+                'useBuiltIns': 'entry',
+                'targets': '>0.2%, not dead, not ie <= 10, not op_mini all'
+              }]
+            ],
+            plugins: [
+              // convert async / await => generators (then generators => regenerator-runtime)
+              '@babel/plugin-transform-async-to-generator',
+              // support for class properties for class components etc
+              '@babel/plugin-proposal-class-properties'
+            ],
+          },
+        },
+      },
+
+      //
+      // *.scss files are processed through three steps:
+      // - style-loader, which includes them in the output bundle and will include a style tag in
+      //   the dom when the component's compiled bundle is loaded.
+      // - css-loader, which does the css modules transformations - this means each class name in
+      //   each file is rewritten so it only targets the elements that are desired.
+      // - sass-loader, which does the sass compilation with node-sass. In addition, this uses
+      //   node-sass-json importer to permit importing of json files containing variables.
+      {
+        test: /\.scss$/,
+        use: [
+          'style-loader',
+          {
+            loader: 'css-loader',
+            options: {
+              importLoaders: 1,
+              modules: true,
+              /* for example, drp-styles-button-1SZwRzu8 */
+              localIdentName: 'drp-[name]-[local]-[hash:base64:8]',
+            },
+          },
+          {
+            loader: 'sass-loader',
+            options: {
+							// Apply the JSON importer via sass-loader's options.
+              importer: jsonImporter,
+							// Tell node-sass to search in the node modules directory for things to import
+							includePaths: [
+								path.join(path.resolve(__dirname), 'node_modules'),
+							],
+            },
+          },
+        ],
+        include: path.resolve(__dirname),
+      },
+      {
+        test: /\.css$/,
+        use: [
+          'style-loader',
+          'css-loader',
+        ],
+        include: path.resolve(__dirname),
+      },
+    ],
+  },
+};
