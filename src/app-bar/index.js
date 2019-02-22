f81089339d73ab376dc8108c844b222332f34f64
Gus Cost <guscost@gmail.com>
2019-02-22 11:51:26 -0500

    code: breaking change to appbar

--- src/app-bar/index.js
+++ src/app-bar/index.js
@@ -1,16 +1,26 @@
 import React from 'react';
 import styles from './styles.scss';
 
-export default function AppBar({
-  leftSpan,
-  rightSpan,
-  title,
-}) {
-  return <div className={styles.appBar}>
-    {leftSpan}
-    <div className={styles.appBarTitle}>{title}</div>
-    {rightSpan}
-  </div>;
+export function AppBarTitle({ children }) {
+  return (
+    <span className={styles.appBarTitle}>
+      {children}
+    </span>
+  );
+}
+
+export function AppBarSection({ children }) {
+  return (
+    <span>{children}</span>
+  );
+}
+
+export default function AppBar({ children }) {
+  return (
+    <div className={styles.appBar}>
+      {children}
+    </div>
+  );
 }
 
 AppBar.displayName = 'AppBar';
--- src/app-bar/story.js
+++ src/app-bar/story.js
@@ -2,9 +2,18 @@ import React from 'react';
 import { storiesOf } from '@storybook/react';
 import { action } from '@storybook/addon-actions';
 
-import AppBar from './';
+import AppBar, {AppBarSection, AppBarTitle} from './';
 
 storiesOf('AppBar', module)
-  .add('With a text body', () => (
-    <AppBar title="Hello world!" />
+  .add('With text', () => (
+    <AppBar>Hello world!</AppBar>
+  ))
+  .add('With left and right sections', () => (
+    <AppBar>
+      <AppBarSection>
+        <span>ICON</span>
+        <AppBarTitle>Title</AppBarTitle>
+      </AppBarSection>
+      <AppBarSection>ASDF</AppBarSection>
+    </AppBar>
   ))
