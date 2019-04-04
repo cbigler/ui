808963c3f2c5d58f8b32a15326a01e6004029bb4
Ryan Gaus <rsg1egoman@gmail.com>
2019-04-04 12:09:12 -0400

    code: port modal from dashboard to density ui

--- src/modal/story.js
+++ src/modal/story.js
@@ -1,79 +1,100 @@
-import React from 'react';
+import React, { Fragment, Component } from 'react';
 import { storiesOf } from '@storybook/react';
 import { action } from '@storybook/addon-actions';
 
-import './styles.scss';
-
 import Modal from './index';
+import AppBar, { AppBarTitle, AppBarSection } from '../app-bar/index';
 import Button from '../button/index';
-import Card, { CardHeader, CardBody } from '../card';
+import fontVariables from '../../variables/fonts.json';
 
+class ModalTriggerer extends Component {
+  state = {
+    view: 'HIDDEN', /* 'TRANSITION_TO_VISIBLE', 'VISIBLE', 'TRANSITION_TO_HIDDEN' */
+  }
+
+  show = () => {
+    this.setState({view: 'TRANSITION_TO_VISIBLE'}, () => {
+      this.setState({view: 'VISIBLE'});
+    });
+  }
+
+  hide = () => {
+    this.setState({view: 'TRANSITION_TO_HIDDEN'}, () => {
+      setTimeout(() => {
+        this.setState({view: 'HIDDEN'});
+      }, 500);
+    });
+  }
+
+  render() {
+    return (
+      <Fragment>
+        <Button onClick={this.show}>Open modal</Button>
+        {['TRANSITION_TO_VISIBLE', 'VISIBLE', 'TRANSITION_TO_HIDDEN'].includes(this.state.view) ?
+            this.props.children(this.state.view === 'VISIBLE', this.hide) : null}
+      </Fragment>
+    );
+  }
+}
 
 storiesOf('Modal', module)
-  .add('With a card inside', () => (
-    <div>
-      foo
-      <Modal onClickBackdrop={action('backdrop click')}>
-        <Card type="modal">
-          <CardHeader>Confirmation</CardHeader>
-          <CardBody>
-            <p style={{margin: 0}}>This token is for a thing. Lorem Ipsum is simply dummy text of the printing and typesetting industry and this is just here to make you believe in aliens.</p>
-            <br />
-            <Button>Yes</Button>
-            <br />
-            <Button>No</Button>
-          </CardBody>
-        </Card>
+  .add('Modal with app bar', () => (
+    <ModalTriggerer children={(visibility, hide) => (
+      <Modal visible={visibility}>
+        <AppBar>
+          <AppBarTitle>
+            My title
+          </AppBarTitle>
+          <AppBarSection>
+            <Button type="primary" onClick={hide}>Close</Button>
+          </AppBarSection>
+        </AppBar>
       </Modal>
-    </div>
+    )} />
   ))
-  .add('With a close button', () => (
-    <div>
-      foo
-      <Modal
-        onClickBackdrop={action('backdrop click')}
-        onClose={action('close button click')}
-      >
-        <Card type="modal">
-          <CardHeader>Confirmation</CardHeader>
-          <CardBody>
-            <p style={{margin: 0}}>This token is for a thing. Lorem Ipsum is simply dummy text of the printing and typesetting industry and this is just here to make you believe in aliens.</p>
-            <br />
-            <Button>Yes</Button>
-            <br />
-            <Button>No</Button>
-          </CardBody>
-        </Card>
+  .add('Modal with fixed size', () => (
+    <ModalTriggerer children={(visibility, hide) => (
+      <Modal width={500} height={300} visible={visibility}>
+        <AppBar>
+          <AppBarTitle>
+            Foo
+          </AppBarTitle>
+          <AppBarSection>
+            <Button type="primary" onClick={hide}>Close</Button>
+          </AppBarSection>
+        </AppBar>
       </Modal>
-    </div>
+    )} />
   ))
-  .add('With height that overflows the viewport', () => (
-    <div>
-      foo
-      <Modal onClickBackdrop={action('backdrop click')}>
-        <Card type="modal">
-          <CardHeader>Confirmation</CardHeader>
-          <CardBody>
-            <p style={{margin: 0}}>This token is for a thing. Lorem Ipsum is simply dummy text of the printing and typesetting industry and this is just here to make you believe in aliens.</p>
-            <p style={{margin: 0}}>This token is for a thing. Lorem Ipsum is simply dummy text of the printing and typesetting industry and this is just here to make you believe in aliens.</p>
-            <p style={{margin: 0}}>This token is for a thing. Lorem Ipsum is simply dummy text of the printing and typesetting industry and this is just here to make you believe in aliens.</p>
-            <p style={{margin: 0}}>This token is for a thing. Lorem Ipsum is simply dummy text of the printing and typesetting industry and this is just here to make you believe in aliens.</p>
-            <p style={{margin: 0}}>This token is for a thing. Lorem Ipsum is simply dummy text of the printing and typesetting industry and this is just here to make you believe in aliens.</p>
-            <p style={{margin: 0}}>This token is for a thing. Lorem Ipsum is simply dummy text of the printing and typesetting industry and this is just here to make you believe in aliens.</p>
-            <p style={{margin: 0}}>This token is for a thing. Lorem Ipsum is simply dummy text of the printing and typesetting industry and this is just here to make you believe in aliens.</p>
-            <p style={{margin: 0}}>This token is for a thing. Lorem Ipsum is simply dummy text of the printing and typesetting industry and this is just here to make you believe in aliens.</p>
-            <p style={{margin: 0}}>This token is for a thing. Lorem Ipsum is simply dummy text of the printing and typesetting industry and this is just here to make you believe in aliens.</p>
-            <p style={{margin: 0}}>This token is for a thing. Lorem Ipsum is simply dummy text of the printing and typesetting industry and this is just here to make you believe in aliens.</p>
-            <p style={{margin: 0}}>This token is for a thing. Lorem Ipsum is simply dummy text of the printing and typesetting industry and this is just here to make you believe in aliens.</p>
-            <p style={{margin: 0}}>This token is for a thing. Lorem Ipsum is simply dummy text of the printing and typesetting industry and this is just here to make you believe in aliens.</p>
-            <p style={{margin: 0}}>This token is for a thing. Lorem Ipsum is simply dummy text of the printing and typesetting industry and this is just here to make you believe in aliens.</p>
-            <p style={{margin: 0}}>This token is for a thing. Lorem Ipsum is simply dummy text of the printing and typesetting industry and this is just here to make you believe in aliens.</p>
-            <br />
-            <Button>Yes</Button>
-            <br />
-            <Button>No</Button>
-          </CardBody>
-        </Card>
+  .add('Modal with onBlur handler', () => (
+    <ModalTriggerer children={(visibility, hide) => (
+      <Modal width={500} height={300} visible={visibility} onBlur={action('onBlur')}>
+        <AppBar>
+          <AppBarTitle>
+            Foo
+          </AppBarTitle>
+          <AppBarSection>
+            <Button type="primary" onClick={hide}>Close</Button>
+          </AppBarSection>
+        </AppBar>
+
+        <p style={{fontFamily: fontVariables.fontBase}}>Click on the backdrop!</p>
       </Modal>
-    </div>
+    )} />
+  ))
+  .add('Modal with onEscape handler', () => (
+    <ModalTriggerer children={(visibility, hide) => (
+      <Modal width={500} height={300} visible={visibility} onEscape={action('onEscape')}>
+        <AppBar>
+          <AppBarTitle>
+            Foo
+          </AppBarTitle>
+          <AppBarSection>
+            <Button type="primary" onClick={hide}>Close</Button>
+          </AppBarSection>
+        </AppBar>
+
+        <p style={{fontFamily: fontVariables.fontBase}}>Press escape!</p>
+      </Modal>
+    )} />
   ))
