c1822b05bca2c8cef0c7de51c0aee37c74501d7a
Ryan Gaus <rsg1egoman@gmail.com>
2019-04-04 14:35:02 -0400

    code: add skeleton component

---
+++ src/skeleton/index.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import propTypes from 'prop-types';
+import styles from './styles.scss';
+import colorVariables from '../../variables/colors.json';
+
+export default function Skeleton({width, height, color}) {
+  return (
+    <span
+      className={styles.skeleton}
+      style={{width, height, backgroundColor: color}}
+    />
+  );
+}
+
+Skeleton.propTypes = {
+  width: propTypes.oneOfType([propTypes.string, propTypes.number]),
+  height: propTypes.oneOfType([propTypes.string, propTypes.number]),
+  color: propTypes.string,
+};
+
+Skeleton.defaultProps = {
+  width: '100%',
+  height: 8,
+  color: colorVariables.grayDarker,
+};
+Skeleton.displayName = 'Skeleton';
---
+++ src/skeleton/story.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import { storiesOf } from '@storybook/react';
+import { action } from '@storybook/addon-actions';
+
+import './styles.scss';
+import Skeleton from './index';
+import Button from '../button/index';
+import colorVariables from '../../variables/colors.json';
+
+import Icons from '../icons';
+
+storiesOf('Skeleton', module)
+  .add('Default skeleton', () => (
+    <Skeleton width={300} />
+  ))
+  .add('Big skeleton', () => (
+    <Skeleton width={300} height={24} />
+  ))
+  .add('Primary-colored skeleton', () => (
+    <Skeleton color={colorVariables.brandPrimary} width={300} height={24} />
+  ))
+  .add('Skeleton in Button', () => (
+    <Button type="primary" disabled>
+      <Skeleton width={100} color="#fff" />
+    </Button>
+  ))
