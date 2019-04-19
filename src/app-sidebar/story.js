a36d3447b27af09bdfd03145a6ab34d1d7311f13
Ryan Gaus <rsg1egoman@gmail.com>
2019-04-19 13:00:50 -0400

    code: add width prop to app sidebar
    This allows a user to specify a wider sidebar than normal.

--- src/app-sidebar/story.js
+++ src/app-sidebar/story.js
@@ -3,11 +3,43 @@ import { storiesOf } from '@storybook/react';
 import { action } from '@storybook/addon-actions';
 
 import AppFrame from '../app-frame';
+import AppPane from '../app-pane';
 import AppSidebar from './';
 
 storiesOf('AppSidebar', module)
-  .add('With a text body', () => (
-    <AppFrame>
-      <AppSidebar visible={true}>Test!</AppSidebar>
-    </AppFrame>
+  .add('Default', () => (
+    <div style={{height: 300}}>
+      <AppFrame>
+        <AppSidebar visible={true}>
+          This is the sidebar
+        </AppSidebar>
+        <AppPane>
+          This is a pane
+        </AppPane>
+      </AppFrame>
+    </div>
+  ))
+  .add('With a bigger width', () => (
+    <div style={{height: 500}}>
+      <AppFrame>
+        <AppSidebar visible={true} width={600}>
+          This is the sidebar
+        </AppSidebar>
+        <AppPane>
+          This is a pane
+        </AppPane>
+      </AppFrame>
+    </div>
+  ))
+  .add('With a smaller width', () => (
+    <div style={{height: 500}}>
+      <AppFrame>
+        <AppSidebar visible={true} width={200}>
+          This is the sidebar
+        </AppSidebar>
+        <AppPane>
+          This is a pane
+        </AppPane>
+      </AppFrame>
+    </div>
   ))
