d2cd412bf537ee1e5df120f5a90bf0dddde34acd
Ryan Gaus <rsg1egoman@gmail.com>
2019-04-23 13:55:03 -0400

    code: fix broken expand collapse functionality in sidebar
    By making the change that permitted a custom app sidebar width, I broke
    the ability for the visible prop to show or hide the sidebar. Added a
    story for this state and fixed the underlying issue.

--- src/app-sidebar/index.js
+++ src/app-sidebar/index.js
@@ -3,9 +3,10 @@ import classnames from 'classnames';
 import styles from './styles.scss';
 
 export default function AppSidebar({ visible, width=415, children }) {
-  return <div className={classnames(styles.appSidebarCollapser, {
-    [styles.visible]: visible
-  })} style={{width}}>
+  return <div
+    className={classnames(styles.appSidebarCollapser, { [styles.visible]: visible })}
+    style={{width: visible ? width : undefined}}
+  >
     <div className={styles.appSidebar} style={{width}}>{children}</div>
   </div>;
 }
--- src/app-sidebar/story.js
+++ src/app-sidebar/story.js
@@ -1,10 +1,11 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { storiesOf } from '@storybook/react';
 import { action } from '@storybook/addon-actions';
 
 import AppFrame from '../app-frame';
 import AppPane from '../app-pane';
 import AppSidebar from './';
+import Button from '../button';
 
 storiesOf('AppSidebar', module)
   .add('Default', () => (
@@ -19,6 +20,30 @@ storiesOf('AppSidebar', module)
       </AppFrame>
     </div>
   ))
+  .add('With expand/collapse functionality', () => {
+    function Wrapper() {
+      const [visible, set] = useState(true);
+      return (
+        <div style={{height: 500}}>
+          <AppFrame>
+            <AppSidebar visible={visible}>
+              This is the sidebar, collapse me by clicking on the button
+            </AppSidebar>
+            <AppPane>
+              <p style={{padding: 12}}>This is a pane</p>
+              <div style={{padding: 12}}>
+                <Button width={200} onClick={() => set(!visible)}>Toggle sidebar</Button>
+              </div>
+            </AppPane>
+          </AppFrame>
+        </div>
+      );
+    }
+
+    return (
+      <Wrapper />
+    );
+  })
   .add('With a bigger width', () => (
     <div style={{height: 500}}>
       <AppFrame>
