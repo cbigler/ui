6f40ef474bbd5fffbd94d3e2eb28899e5ac079f3
Gus Cost <guscost@gmail.com>
2020-04-06 18:21:11 -0400

    code: standard inputs for start/end times

--- src/time-filter-picker/index.tsx
+++ src/time-filter-picker/index.tsx
@@ -4,15 +4,30 @@ import moment from 'moment-timezone';
 import MomentUtils from '@date-io/moment';
 import { MuiPickersUtilsProvider, KeyboardTimePicker } from '@material-ui/pickers';
 
+import InputBox from '../input-box';
+
+function TimePickerInput({value, onChange, disabled, error}) {
+  return <InputBox width={108} value={value} onChange={onChange} disabled={disabled} invalid={error} />;
+}
+
+function TimePicker({value, onChange}) {
+  return <KeyboardTimePicker
+    mask="__:__ _M"
+    placeholder="08:00 AM"
+    value={value}
+    onChange={onChange}
+    TextFieldComponent={TimePickerInput} />;
+}
+
 function TimeFilterPicker() {
-  const [timePickerValue, setTimePickerValue] = useState(moment());
+  const [startTimePickerValue, setStartTimePickerValue] = useState(moment('08:00', 'hh:mm'));
+  const [endTimePickerValue, setEndTimePickerValue] = useState(moment('17:00', 'hh:mm'));
   return <MuiPickersUtilsProvider utils={MomentUtils}>
     <div style={{width: '100%'}}>
-      <KeyboardTimePicker
-        placeholder="08:00 AM"
-        mask="__:__ _M"
-        value={timePickerValue}
-        onChange={setTimePickerValue} />
+      <div style={{display: 'flex'}}>
+        <TimePicker value={startTimePickerValue} onChange={setStartTimePickerValue} />
+        <TimePicker value={endTimePickerValue} onChange={setEndTimePickerValue} />
+      </div>
     </div>
   </MuiPickersUtilsProvider>;
 }
