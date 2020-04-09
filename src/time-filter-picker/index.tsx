9b1c713f588bafcc57c68035e5d523130a9660ef
Gus Cost <guscost@gmail.com>
2020-04-09 16:23:53 -0400

    code: today/tomorrow mode

--- src/time-filter-picker/index.tsx
+++ src/time-filter-picker/index.tsx
@@ -21,25 +21,56 @@ function TimePicker({value, onChange}) {
     TextFieldComponent={TimePickerInput} />;
 }
 
-function TimeFilterDisplay({displayTwoDays, shadedStartPercent, shadedEndPercent, color=colorVariables.blue}) {
-  return <div style={{display: 'flex', position: 'relative', flex: 1}}>
-    <div style={{width: '100%', height: 5, backgroundColor: colorVariables.gray300}}></div>
-    <div style={{
-      position: 'absolute',
-      top: 0,
-      height: 5,
-      left: `${shadedStartPercent}%`,
-      width: `${shadedEndPercent}%`,
-      backgroundColor: color
-    }}></div>
-    <div style={{marginTop: 8}}>
-      {(displayTwoDays ? [0,4,8,12,16,20,24] : [0,2,4,6,8,10,12,14,16,18,20,22,24]).map(hours => (
-        <div key={hours} style={{
-          position: 'absolute',
-          left: `${hours * 100 / 24}%`,
-          transform: 'translateX(-50%)'
-        }}>{moment().startOf('day').add(hours, 'hours').format('ha').slice(0, -1)}</div>
-      ))}
+function TimeFilterDisplay({displayTwoDays, shadedStartPercent, shadedWidthPercent, isTomorrow}) {
+  return <div style={{
+    flex: 1,
+    paddingTop: displayTwoDays ? 12 : 24,
+    paddingBottom: 56,
+    borderRight: (displayTwoDays && !isTomorrow) ? `1px dashed ${colorVariables.gray300}` : undefined,
+  }}>
+    {displayTwoDays ? <div style={{
+        display: 'flex',
+        color: colorVariables.gray400,
+        fontSize: 11,
+        fontWeight: 500,
+        marginBottom: 16,
+      }}>
+        {isTomorrow ? <div style={{flex: 1, marginLeft: 12}}>TOMORROW</div> : <div style={{flex: 1}}>TODAY</div>}
+      </div> : null}
+    <div style={{position: 'relative'}}>
+      <div style={{
+        width: '100%',
+        height: 5,
+        backgroundColor: colorVariables.gray300,
+        borderTopLeftRadius: displayTwoDays ? isTomorrow ? 0 : 3 : 3,
+        borderBottomLeftRadius: displayTwoDays ? isTomorrow ? 0 : 3 : 3,
+        borderTopRightRadius: displayTwoDays ? isTomorrow ? 3 : 0 : 3,
+        borderBottomRightRadius: displayTwoDays ? isTomorrow ? 3 : 0 : 3,
+      }}></div>
+      <div style={{
+        position: 'absolute',
+        top: 0,
+        height: 5,
+        left: `${shadedStartPercent}%`,
+        width: `${isNaN(shadedWidthPercent) ? 0 : shadedWidthPercent}%`,
+        backgroundColor: isTomorrow ? colorVariables.yellow : colorVariables.blue,
+        borderRadius: 4,
+      }}></div>
+      <div style={{marginTop: 8}}>
+        {(displayTwoDays ? isTomorrow ? [0,4,8,12,16,20,24] : [0,4,8,12,16,20] : [0,2,4,6,8,10,12,14,16,18,20,22,24]).map(hours => {
+          const tickPercent = hours * 100 / 24;
+          return <div key={hours} style={{
+            position: 'absolute',
+            left: `${tickPercent}%`,
+            transform: (isTomorrow && hours === 0) ? undefined : 'translateX(-50%)',
+            fontSize: 12,
+            fontWeight: 500,
+            color: (tickPercent >= shadedStartPercent && tickPercent <= shadedStartPercent + shadedWidthPercent) ?
+              colorVariables.gray400 :
+              colorVariables.gray200
+          }}>{moment().startOf('day').add(hours, 'hours').format('ha').slice(0, -1)}</div>;
+        })}
+      </div>
     </div>
   </div>
 }
@@ -51,10 +82,17 @@ function TimeFilterPicker() {
 
   let displayTwoDays = false;
   let endTimeNormalized = endTimePickerValue && endTimePickerValue.clone();
-  while (endTimeNormalized && endTimeNormalized.diff(startTimePickerValue, 'days') > 0) {
+  while (
+    endTimeNormalized &&
+    endTimeNormalized.diff(startTimePickerValue, 'days') > 0
+  ) {
     endTimeNormalized.subtract(1, 'day');
   }
-  if (endTimeNormalized.diff(startTimePickerValue) <= 0 && endTimeNormalized !== endTimeNormalized.clone().startOf('day')) {
+  if (
+    endTimeNormalized &&
+    endTimeNormalized.diff(startTimePickerValue) <= 0 &&
+    endTimeNormalized !== endTimeNormalized.clone().startOf('day')
+  ) {
     endTimeNormalized.add(1, 'day');
     displayTwoDays = true;
   }
@@ -63,13 +101,17 @@ function TimeFilterPicker() {
   const startOfDay = startTimePickerValue.clone().startOf('day');
   const shadedStartPercent = startTimePickerValue ? 
     startTimePickerValue.diff(startOfDay, 'minutes') * 100 / 1440 : 0;
-  const shadedEndPercent = (startTimePickerValue && endTimeNormalized) ? 
+  const shadedWidthPercent = (startTimePickerValue && endTimeNormalized) ? 
     displayTwoDays ? 
       endTimeNormalized.diff(endTimeNormalized.clone().startOf('day'), 'minutes') * 100 / 1440 :
       endTimeNormalized.diff(startTimePickerValue, 'minutes') * 100 / 1440 : 0;
 
   return <MuiPickersUtilsProvider utils={MomentUtils}>
-    <div style={{padding: 24}}>
+    <div style={{
+      paddingTop: 16,
+      paddingLeft: 16,
+      paddingRight: 16,
+    }}>
       <div style={{display: 'flex'}}>
         <TimePicker value={startTimePickerValue} onChange={setStartTimePickerValue} />
         <div style={{lineHeight: '40px', padding: '0px 8px'}}>to</div>
@@ -83,18 +125,21 @@ function TimeFilterPicker() {
         width: 'calc(100% + 48px)',
         height: 1,
         backgroundColor: colorVariables.gray300,
-        margin: '16px -24px'
+        marginTop: 16,
+        marginLeft: -24,
+        marginRight: -24,
       }}></div>
       <div style={{display: 'flex'}}>
         <TimeFilterDisplay
           displayTwoDays={displayTwoDays}
           shadedStartPercent={shadedStartPercent}
-          shadedEndPercent={displayTwoDays ? 100 : shadedEndPercent} />
+          shadedWidthPercent={displayTwoDays ? 100 - shadedStartPercent : shadedWidthPercent}
+          isTomorrow={false} />
         {displayTwoDays ? <TimeFilterDisplay
           displayTwoDays={displayTwoDays}
           shadedStartPercent={0}
-          shadedEndPercent={shadedEndPercent}
-          color={colorVariables.yellow} /> : null}
+          shadedWidthPercent={shadedWidthPercent}
+          isTomorrow={true} /> : null}
       </div>
     </div>
   </MuiPickersUtilsProvider>;
